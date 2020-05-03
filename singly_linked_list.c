f6a552b3befefbfe708c6e7730685cd040810b36
ricardosn <ricardorichsn@gmail.com>
2020-05-03 10:44:24 -0300

    Add Append function

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// malloc aloca memória e retorna o ponteiro para o primeiro Byte
+
 struct Node {
     int data;
     struct Node *next;
 };
 
-void push(struct Node** head_ref, int data) {
-    struct Node* new = (struct Node *) malloc(sizeof(struct Node));
+void push(struct Node **head_ref, int data) {
+    struct Node *new = (struct Node *) malloc(sizeof(struct Node));
 
     new->data = data;
     new->next = (*head_ref);
 
     (*head_ref) = new;
+}
+
+void append(struct Node **head_ref, int data) {
+    struct Node *new = (struct Node *) malloc(sizeof(struct Node));
+    struct Node *last = (*head_ref);
+
+    new->data = data;
+    new->next = NULL;
+
+    if (*head_ref == NULL) {
+        (*head_ref) = new;
+        return;
+    }
+
+    // Last Node
+    while(last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = new;
 }
 
 void printList(struct Node *node) {
@@ -29,6 +51,8 @@ int main() {
     push(&head, 7);
 
     push(&head, 8);
+
+    append(&head, 10);
 
     printf("\n Created Linked list is: ");
 
